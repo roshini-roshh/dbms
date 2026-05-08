38aa464955125d427dd7e9f92cadee5914ad6282
roshini-roshh <roshniisivakumar@gmail.com>
2026-05-08 07:26:14 +0530

    Create depttop3sal.sql

---
+++ EX4/leetcode/depttop3sal.sql
@@ -0,0 +1,17 @@
+SELECT Department,
+Employee,
+Salary
+FROM (
+    SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary,
+    DENSE_RANK() OVER(
+        PARTITION BY d.id
+        ORDER BY e.salary DESC
+    ) rnk
+    FROM Employee e
+    JOIN Department d
+    ON e.departmentId = d.id
+)
+WHERE rnk <= 3;
